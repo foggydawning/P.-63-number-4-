f845159a86dd7900d2d4c5ee02ff99dca43d3218
ILYA CHUB <iliachubich@gmail.com>
2019-05-19 01:27:55 +0300

    добавлена функция поиска в массиве максимаьной длины последовательности из одинаковых элементов

--- program.py
+++ program.py
@@ -3,4 +3,13 @@ def add_random_list (list_=[]):
     for i in range (0,21):
         list_.append(random.randint(10,12))
     return list_
-print(add_random_list())
+
+def poisk (list, max=1):
+    for i in range (0, len(list)-1):
+        print(i,max)
+        k=1
+        while i+k < len (list) and list[i]==list[i+k]:
+            k+=1
+        if max<k:
+            max=k
+    return max
